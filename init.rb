9a505609ce4e1d4549ad35894421eb672ffcb60c
Jamison Dance <jergason@gmail.com>
2010-12-03 11:37:26 -0700

    other changes

--- init.rb
+++ init.rb
@@ -1,10 +1,11 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "lib"))
 require "configuration"
+require "yaml"
 
 
 # Edit configuration settings here
 config = Scheduler::Configuration.new
 config.email = "jergason@byu.net"
-config.yaml_location = "queue.yaml"
+config.queue_location = "queue.yaml"
 config.calendar_html = "<foo>bahldlksjf</foo>"
 
